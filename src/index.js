d566a69dfeacfe8a3fb3182e9a7efffa8f55489d
Shiyu Wang <shiyu3169@gmail.com>
2018-08-07 14:01:54 -0400

    tabs and pills

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import '../node_modules/bootstrap/dist/css/bootstrap.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import '../node_modules/font-awesome/css/font-awesome.min.css';
+import LessonTabs from './LessonTabs'
+import TopicPills from './TopicPills'
 
 class ModuleItem extends React.Component {
 	render() {
 		return (
-			<li className="list-group-item">{this.props.title}</li>
+			<li className="list-group-item">
+				{this.props.title}
+				<span className="float-right">
+					<i style={{"marginRight":"5px"}} className="fa fa-trash"></i>
+					<i className="fa fa-pencil"></i>
+				</span>
+			</li>
 		)
 	}
 }
@@ -30,12 +39,12 @@ class CourseCard extends React.Component {
 	render() {
 		return (
 			<div className="card" style={{"width": "18rem"}}>
-				<img className="car-img-top" src="https://picsum.photos/300/200"/>
+				<img className="car-img-top" alt="random img" src="https://picsum.photos/300/200"/>
 			  	<div className="card-body">
 				    <h5 className="card-title">Card title</h5>
 				    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-				    <a href="#" className="card-link">Card link</a>
-				    <a href="#" className="card-link">Another link</a>
+				    <a href="" className="card-link">Card link</a>
+				    <a href="" className="card-link">Another link</a>
 			  	</div>
 			</div>
 		)
@@ -47,6 +56,8 @@ class WhiteBoard extends React.Component {
 		return (
 			<div className="container-fluid">
 				<h1>Whiteboard</h1>
+				<TopicPills/>
+				<LessonTabs/>
 				<ModuleList/>
 				<div className="card-deck">
 					<CourseCard/>
