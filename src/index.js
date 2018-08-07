f71ebce53de004f7f4b5c678b06eb38e037bb3ab
Shiyu Wang <shiyu3169@gmail.com>
2018-08-07 16:33:21 -0400

    form

--- src/index.js
+++ src/index.js
@@ -14,25 +14,26 @@ const ModuleListItemStateLess = ({title}) =>
 			</span>
 		</li>
 
-class ModuleItem extends React.Component {
-	render() {
-		return (
-			<li className="list-group-item">
-				{this.props.title}
-				<span className="float-right">
-					<i style={{"marginRight":"5px"}} className="fa fa-trash"></i>
-					<i className="fa fa-pencil"></i>
-				</span>
-			</li>
-		)
-	}
-}
+// class ModuleItem extends React.Component {
+// 	render() {
+// 		return (
+// 			<li className="list-group-item">
+// 				{this.props.title}
+// 				<span className="float-right">
+// 					<i style={{"marginRight":"5px"}} className="fa fa-trash"></i>
+// 					<i className="fa fa-pencil"></i>
+// 				</span>
+// 			</li>
+// 		)
+// 	}
+// }
 
 class ModuleList extends React.Component {
 
 	constructor() {
 		super();
 		this.state = {
+			title: '',
 			modules: [
 				{title: "Module 1"},
 				{title: "Module 2"},
@@ -42,15 +43,35 @@ class ModuleList extends React.Component {
 		}
 	}
 
+	titleChanged = (e) => {
+		// console.log(e.target.value);
+		this.setState({title: e.target.value});
+	}
+
+	createModule = () => {
+		const module = {title: this.state.title};
+		this.state.modules.push(module);
+		this.setState({modules: this.state.modules});
+	}
+
+	renderModuleList() {
+		let modules = 
+			this.state.modules.map(
+				(module, i) => <ModuleListItemStateLess key={i} title={module.title} />
+			)
+		return modules;
+	}
+
 	render() {
 		return (
 			<div>
 				<h1>Module List</h1>
+				<h2>{this.state.title}</h2>
+				<input className="form-control" placeholder="title" onChange={this.titleChanged}/>
+				<button onClick={this.createModule} className="btn btn-primary btn-block">Add Module</button>
 				<ul className="list-group">
 					{
-						this.state.modules.map(
-							(module, i) => <ModuleListItemStateLess key={i} title={module.title} />
-						)
+						this.renderModuleList()
 					}
 				</ul>
 			</div>
