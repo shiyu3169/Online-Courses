c0447e5ae8262c6b1d042045bab93a0133eaeeb0
Shiyu Wang <shiyu3169@gmail.com>
2018-09-06 11:24:51 -0400

    create widget type

--- src/containers/widgets/WidgetListComponent.js
+++ src/containers/widgets/WidgetListComponent.js
@@ -5,6 +5,7 @@ import { WidgetType3 } from './WidgetType3';
 
  const WidgetListComponent = ({widgets, deleteWidget, createWidget}) => {
     let widgetTitle; 
+    let widgetType;
     return (
         <div>
             <h1>Widget List ({widgets.length})</h1>
@@ -12,16 +13,23 @@ import { WidgetType3 } from './WidgetType3';
                 <li className="list-group-item">
                     <input className="form-control" ref={(node) => {widgetTitle = node}}/>
                     <button className="btn btn-success" 
-                    onClick={
-                        () => {
-                            let widget = {
-                                title: widgetTitle.value, 
-                                id: (new Date()).getTime()
-                            };
-                            widgetTitle.value = ''
-                            createWidget(widget)
-                        }
-                        }>Add widget</button>
+                            onClick={
+                                () => {
+                                    let widget = {
+                                        title: widgetTitle.value, 
+                                        id: (new Date()).getTime(),
+                                        widgetType: widgetType.value
+                                    };
+                                    widgetTitle.value = ''
+                                    createWidget(widget)
+                                }}>
+                        Add widget
+                    </button>
+                    <select className="form-control" ref={(node) => {widgetType = node}}>
+                        <option value="WT1">Widget Type 1</option>
+                        <option value="WT2">Widget Type 2</option>
+                        <option value="WT3">Widget Type 3</option>
+                    </select>
                 </li>
                 {widgets.map((widget,index)=>
                     <li className="list-group-item" key={index}>
